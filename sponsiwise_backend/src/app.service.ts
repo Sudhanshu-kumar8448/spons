0262473bff98895cba44835e9b7f03d7aa409214
Sudhanshu Kumar <sudhanshukumar8448@gmail.com>
2026-02-20 18:41:37 +0530

    check

--- sponsiwise_backend/src/app.service.ts
+++ sponsiwise_backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Sponsiwise Backend is running';
+    return 'Sponsiwise Backend is running check things are runnig or not';
   }
 }
